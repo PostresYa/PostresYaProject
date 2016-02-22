32ae2da4e950f873f05dd1505267cd251ce1bf81
duvanbarrera <duvan.barrera@mail.escuelaing.edu.co>
2016-02-22 18:32:33 -0500

    pruebas correctasgit add .git add .

--- src/test/java/edu/eci/cosw/postresYa/PostresYaTest.java
+++ src/test/java/edu/eci/cosw/postresYa/PostresYaTest.java
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PostresYaMain.class)
-public class PostresYaTests {
+public class PostresYaTest {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoadsTest() {
 	}
 
 }
