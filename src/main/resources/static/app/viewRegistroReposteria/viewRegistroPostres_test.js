b003dfa995821e21c7501ae5278a04d8ab2fcf84
diegorocr0418 <diegorocr0418@gmail.com>
2016-03-26 12:37:46 -0500

    Vista registro de reposteria

---
+++ src/main/resources/static/app/viewRegistroReposteria/viewRegistroPostres_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.viewRegistroPostres module', function() {
+
+  beforeEach(module('myApp.viewRegistroPostres'));
+
+  describe('view1 controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var view1Ctrl = $controller('ViewRegistroPostresCtrl');
+      expect(view1Ctrl).toBeDefined();
+    }));
+
+  });
+});
