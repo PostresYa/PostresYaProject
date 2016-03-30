187c7ff6dfbb17560dc870a12ef6b4ff6ed9e072
duvanbarrera <duvan.barrera@mail.escuelaing.edu.co>
2016-03-30 16:24:13 -0500

    Update PostresYaTest.java

--- src/test/java/edu/eci/cosw/postresYa/PostresYaTest.java
+++ src/test/java/edu/eci/cosw/postresYa/PostresYaTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = PostresYaTest.class)
+@SpringApplicationConfiguration(classes = PostresYaMain.class)
 @WebAppConfiguration
 public class PostresYaTest {
 
