615a52852d21b075f80a5af5d88c389204a6b407
duvanbarrera <duvan.barrera@mail.escuelaing.edu.co>
2016-04-03 19:20:36 -0500

    esqueleto persistencia

--- src/test/java/edu/eci/cosw/postresYa/PostresYaTest.java
+++ src/test/java/edu/eci/cosw/postresYa/PostresYaTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PostresYaMain.class)
 @WebAppConfiguration
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
 public class PostresYaTest {
 
       
