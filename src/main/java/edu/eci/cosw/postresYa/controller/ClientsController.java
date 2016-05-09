b5b65f0457dbbe47fba5c3d761a6424d7b1e073e
duvanbarrera <duvan.barrera@mail.escuelaing.edu.co>
2016-05-08 22:34:45 -0500

    autenticacion clientes

---
+++ src/main/java/edu/eci/cosw/postresYa/controller/ClientsController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cosw.postresYa.controller;
+
+import javax.annotation.security.RolesAllowed;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author duvan
+ */
+@RestController
+@RequestMapping(value="/cliente")
+public class ClientsController {
+    @RequestMapping(value="/check")
+   
+    @RolesAllowed("cliente")
+    public ResponseEntity<?> check(){
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+}
